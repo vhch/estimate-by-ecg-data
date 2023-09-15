34c843c2a6874a3947042cea8596aead69bb94e9
vhch <vhch66@gmail.com>
2023-09-15 19:30:48 +0000

    1

--- main2.py
+++ main2.py
@@ -4,12 +4,13 @@ import numpy as np
 from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
-from concurrent.futures import ProcessPoolExecutor
+
+BATCH_SIZE = 1000
 
 csv_path_adult = 'dataset/ECG_adult_age_train.csv'
 csv_path_child = 'dataset/ECG_child_age_train.csv'
 numpy_folder_adult = 'dataset/adult/train/'
-numpy_folder_child = 'dataset/child/train/'  # 두 번째 numpy 폴더 경로
+numpy_folder_child = 'dataset/child/train/'
 
 # CSV 파일 불러오기
 df1 = pd.read_csv(f'{csv_path_adult}')
@@ -19,41 +20,43 @@ df2 = pd.read_csv(f'{csv_path_child}')
 df1['FOLDER'] = numpy_folder_adult
 df2['FOLDER'] = numpy_folder_child
 
+gender_mapping = {'male': 1, 'female': 0}  # 예를 들어 이렇게 매핑. 실제 데이터에 맞게 조정해야 합니다.
+df1['GENDER'] = df1['GENDER'].map(gender_mapping)
+df2['GENDER'] = df2['GENDER'].map(gender_mapping)
+
 # 두 데이터프레임 합치기
 df = pd.concat([df1, df2], ignore_index=True)
 
-# 메모리에 모든 npy 파일 로드하기
-file_data = {}
-for filename in df['FILENAME']:
-    if filename in file_data:  # 이미 로딩한 경우 스킵
-        continue
+def data_generator(df):
+    n = len(df)
+    
+    for start in range(0, n, BATCH_SIZE):
+        end = min(start + BATCH_SIZE, n)
+        subset = df.iloc[start:end]
+        
+        data = []
+        for _, row in subset.iterrows():
+            filename = row['FILENAME']
+            folder = row['FOLDER']
+            path = os.path.join(folder, f"{filename}.npy")
+            data.append(np.load(path).flatten())
+        
+        batch_df = pd.DataFrame(data)
+        X = batch_df
+        y = subset['AGE']
+        
+        yield X, y
 
-    path = os.path.join(numpy_folder_adult, f"{filename}.npy")
-    if not os.path.exists(path):  # 다른 폴더에서 찾기
-        path = os.path.join(numpy_folder_child, f"{filename}.npy")
-    file_data[filename] = np.load(path).flatten()
+gen = data_generator(df)
 
-# 메모리에 로딩한 데이터를 사용하여 DataFrame 생성하기
-data = [file_data[filename] for filename in df['FILENAME']]
-numpy_df = pd.DataFrame(data)
-
-# 원본 df와 numpy_df를 연결
-df = pd.concat([df, numpy_df], axis=1)
-
-# 필요없는 'FILENAME' 및 'FOLDER' 열을 제거
-df.drop(columns=['FILENAME', 'FOLDER'], inplace=True)
-
-# 데이터 분할
-X = df.drop(columns=['AGE'])
-y = df['AGE']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
-
-# 모델 학습
 model = XGBRegressor(objective='reg:squarederror')
-model.fit(X_train, y_train)
 
-# 예측 및 성능 확인
+# 초기 데이터 분할
+X_train, X_test, y_train, y_test = train_test_split(df.drop(columns=['FILENAME', 'FOLDER', 'AGE']), df['AGE'], test_size=0.1, random_state=42)
+
+for batch_X, batch_y in gen:
+    model.fit(batch_X, batch_y, eval_set=[(X_test, y_test)], early_stopping_rounds=10, verbose=False)
+
 y_pred = model.predict(X_test)
 mae = mean_absolute_error(y_test, y_pred)
 print(f'Mean Absolute Error: {mae}')
